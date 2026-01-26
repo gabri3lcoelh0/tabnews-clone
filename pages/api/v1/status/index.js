8ad837587c2a265949010b950959f0131b9c639d
Gabriel Henrique <90316392+ghenrique7@users.noreply.github.com>
2026-01-26 17:02:47 +0000

    add docker and `.env`files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(request, response) {
-  return response.status(200).json({ chave: "deu tudo certo" });
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1+1 AS soma");
+  console.log("Resultado da consulta:", result.rows[0]);
+  return response.status(200).json({ chave: "são acima da média." });
 }
 
 export default status;
