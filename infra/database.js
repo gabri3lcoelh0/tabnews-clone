79bc1f9eaf1ae7d4f952f4d24893a88d7197df75
Gabriel Henrique <90316392+ghenrique7@users.noreply.github.com>
2026-01-30 17:48:50 +0000

    removi codigos de console

--- infra/database.js
+++ infra/database.js
@@ -10,21 +10,11 @@ async function query(queryObject) {
     ssl: getSSLValues(),
   });
 
-  console.error({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    ssl: getSSLValues(),
-  });
-
   try {
     await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch (err) {
-    console.error(err);
     throw err;
   } finally {
     await client.end();
@@ -40,5 +30,5 @@ function getSSLValues() {
     };
   }
 
-  return process.env.NODE_ENV === "development" ? true : true;
+  return process.env.NODE_ENV === "development" ? false : true;
 }
