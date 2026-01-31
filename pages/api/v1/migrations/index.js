33c0323df219f9961be725133e5b619e4090c848
Gabriel Henrique <90316392+ghenrique7@users.noreply.github.com>
2026-01-31 00:00:31 +0000

    Basicamente é feita a mudança no index para lidar com requisições tanto GET como POST e ao receber essas requisições a "unica" mudança até então foi que o dry run é setado para false no POST

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -2,7 +2,8 @@ import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
 
 export default async function migrations(request, response) {
-  const migrations = await migrationRunner({
+ if(request.method === 'GET') {
+   const migrations = await migrationRunner({
     databaseUrl: process.env.DATABASE_URL,
     dir: join("infra", "migrations"),
     direction: "up",
@@ -11,4 +12,19 @@ export default async function migrations(request, response) {
     verbose: true,
   });
   response.status(200).json(migrations);
+ }
+
+ if(request.method === 'POST') {
+   const migrations = await migrationRunner({
+    databaseUrl: process.env.DATABASE_URL,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    migrationsTable: "pgmigrations",
+    dryRun: false,
+    verbose: true,
+  });
+  response.status(200).json(migrations);
+ }
+
+
 }
